4221774e10e00d483632933b232124cf219768ba
Mpumelelo Khuzwayo <Mpumelelo.Khuzwayo@investec.co.za>
2018-02-01 11:40:16 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public string Hello()
+        {
+            return "Hello, World";
+        }
+    }
+}
---
+++ HelloWorldTest/HellTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldTest
+{
+    [TestClass]
+    public class HellTest
+    {
+        [TestMethod]
+        public void TestHello()
+        {
+            HelloWorld.Program hello = new HelloWorld.Program();
+            string helloMessage = hello.Hello();
+
+            Assert.AreEqual("Hello, World", helloMessage);
+        }
+    }
+}
